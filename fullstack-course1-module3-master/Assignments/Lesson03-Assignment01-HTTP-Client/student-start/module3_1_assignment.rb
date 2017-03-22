b458dc5cd2377a0ee8c70387bd8d5f0d9d2dc5bb
Maxwell Frederickson <maxwellfrederickson@gmail.com>
2017-03-22 16:58:50 -0500

    finished API homework, passes 100% of tests

--- fullstack-course1-module3-master/Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ fullstack-course1-module3-master/Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,17 @@
 require 'httparty'
 
 class Recipe
+  include HTTParty
+  base_uri 'http://food2fork.com/api'
+  default_params key: ENV["FOOD2FORK_KEY"], recipes: "recipes"
+  format :json
+
+  def self.for(ingredient)
+    # issues an HTTP GET request using the HTTParty gem
+    # the HTTP GET request must have the "q=keyword" query argument and query "/search" route
+    # returns the JSON payload document supplied in the recipes element of the hash returned by HTTParty
+    recipes_returned = get("/search", query: "q=#{ingredient}")["recipes"]
+    #Array(chocolates)
+  end
 
 end
